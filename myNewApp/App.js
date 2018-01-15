19ef02a6a26f718d677a3a89e095fa4d18c7e6fb
gunjankothari <gunjan.kothari2002@gmail.com>
2018-01-16 00:00:01 +0530

    third

--- myNewApp/App.js
+++ myNewApp/App.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import Voice from 'react-native-voice';
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    constructor(props){
+        super(props);
+        //Voice.onSpeechStart = this.onSpeechStartHandler.bind(this);
+        // Voice.onSpeechEnd = this.onSpeechEndHandler.bind(this);
+        Voice.onSpeechResults = this.onSpeechResultsHandler.bind(this);
+        this.state = {
+            text:'Hello',
+            recognisedText:'',
+            eve: 'none'
+        };
+    }
+
+    onSpeechResultsHandler(event){
+        this.setState({ 'recognisedText': event.value[0] });
+        this.setState({ 'eve': 'result' });
+    }
+
+    onPressStart() {
+        Voice.start('en');
+        this.setState({ 'text': 'Starting' });
+    }
+    onPressStop(){
+        Voice.stop();
+        this.setState({ 'text': 'Stopped' });
+    }
+
+    render() {
+        let { text, recognisedText, eve } = this.state;
+        return (
+          <View style={styles.container}>
+              <Button onPress={ this.onPressStart.bind(this) }
+                      title="Start"
+                      color="#841584" />
+              <Button onPress={ this.onPressStop.bind(this) }
+                      title="Stop"
+                      color="#841584"></Button>
+            <Text>Speech: { recognisedText }</Text>
+            <Text>Listing: { text }</Text>
+            <Text>Event: { eve }</Text>
+          </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -18,6 +53,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
